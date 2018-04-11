86bd98eeae380b1e8966daa0bd167b8204ff7bc1
Ian Klek <KlekI@randa.org>
2018-04-11 19:27:32 +0100

    Avoid duplicate positioning test now passing

--- ChessProject-Csharp/src/ChessBoard.cs
+++ ChessProject-Csharp/src/ChessBoard.cs
@@ -23,13 +23,21 @@ namespace SolarWinds.MSP.Chess
                 pawn.YCoordinate = yCoordinate;
                 pawn.ChessBoard = this;
             }
+            else
+            {
+                pawn.XCoordinate = -1;
+                pawn.YCoordinate = -1;
+            }
             
         }
 
+        //Are co-ords legal and is the spot vacant
         public bool IsLegalBoardPosition(int xCoordinate, int yCoordinate)
         {
             if (xCoordinate < 0 || xCoordinate > MaxBoardHeight) return false;
             if (yCoordinate < 0 || yCoordinate > MaxBoardWidth) return false;
+
+            if (pieces[xCoordinate, yCoordinate] != null) return false;
 
             return true;
         }
