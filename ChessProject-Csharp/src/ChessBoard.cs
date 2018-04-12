f1a7e4de38b0bc1ad3abb597856e8453b40849ee
Ian Klek <KlekI@randa.org>
2018-04-12 15:19:25 +0100

    Limits the number of pieces on the board

--- ChessProject-Csharp/src/ChessBoard.cs
+++ ChessProject-Csharp/src/ChessBoard.cs
@@ -13,10 +13,30 @@ namespace SolarWinds.MSP.Chess
             pieces = new Piece[MaxBoardWidth + 1, MaxBoardHeight + 1];
         }
 
+        /// <summary>
+        /// Counts all the chessboard pieces of the given type 
+        /// </summary>
+        /// <param name="piece">Piece type to count</param>
+        /// <returns></returns>
+        public int CountOfPieces(Piece piece)
+        {
+            int count = 0;
+            for (int x = 0; x <= MaxBoardWidth; x++)
+            {
+                for (int y = 0; y < MaxBoardHeight; y++)
+                {
+                    if (pieces[x, y] != null &&
+                        pieces[x, y].GetType() == piece.GetType())
+                        count++;
+                }
+            }
+            return count;
+        }
 
         public void Add(Piece piece, int xCoordinate, int yCoordinate, PieceColor pieceColor)
         {
-            if (IsLegalBoardPosition(xCoordinate, yCoordinate))
+            if (IsLegalBoardPosition(xCoordinate, yCoordinate) &&
+                CountOfPieces(piece) < piece.MaxPieceCount)
             {
                 pieces[xCoordinate, yCoordinate] = piece;
                 piece.XCoordinate = xCoordinate;
